66f7767b1ca36749bcb3d7dc28ebe5376d65666a
Xavi Ablaza <xavi@hostari.com>
2025-02-08 21:10:30 +0700

    Remove scrollable logo overlay on front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 // @ts-ignore
 import Header from "./_components/header";
-import Image from "next/image";
-import logo from "@/public/logo.png";
 import "./landing-page.css";
-import { Divider } from "@/components/ui/divider";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -34,30 +31,15 @@ export default function LandingPage() {
             className="pb-[5rem] bg-transparent"
           />
         </div>
-        <div className="w-full h-[100px] bg-[#3f2212] opacity-0 animate-fade-in transition-opacity duration-300 ease-in-out" />
-        <div className="tracking-wider hidden md:flex text-3xl text-[#BDBDBD80] animate-fade-in-from-bottom font-serif uppercase absolute z-50 top-[9%] w-[55%] justify-between items-center">
-          <span>Exclusive</span>
-          <span className="absolute left-[50%] -translate-x-2/4">&</span>
-          <span>Craftsmanship</span>
-        </div>
-        <Image src={logo} alt="logo" className="w-full animate-fade-in" />
-        <div className="w-full h-[500px] bg-[#3f2212] opacity-0 animate-fade-in transition-opacity duration-300 ease-in-out relative bottom-[6px]">
-          <div className="flex flex-col justify-center items-center">
-            <h1 className="text-[#BDBDBD80] text-3xl md:text-4xl font-serif uppercase pt-[11rem] px-2">
-              Welcome to the world <br /> of don marrón
-            </h1>
-            <Divider className="bg-[#BDBDBD80] w-[170px]" />
-          </div>
-        </div>
 
         <div className="flex flex-col justify-center items-center pt-[11rem] h-screen animate-fade-in">
-
           <Button className="mt-8">
             <Link href="/sign-up" className="uppercase w-[300px]">
               Apply for Membership
             </Link>
           </Button>
         </div>
+
         <div className="absolute bottom-[60px] md:bottom-[40px] left-[15px] md:left-[50px] p-4">
           <Link
             href="/learn-more"
